a86f2ddcf155513db3873a279519f4a2dea7264d
Cosmo594 <292253112@qq.com>
2020-07-26 01:09:03 +0800

    20200726

--- demo-天知道/js/main.js
+++ demo-天知道/js/main.js
@@ -11,23 +11,24 @@
  var app = new Vue({
      el:"#app",
      data:{
-         city:'',
+         city:"",
          weatherList:[]
      },
-     methods: {
+     methods:{
          searchWeather:function(){
-            //  console.log('天气查询');
+            //  console.log("天气查询");
             //  console.log(this.city);
-            // 调用接口
-            // 保存this
             var that = this;
-            axios.get('http://wthrcdn.etouch.cn/weather_mini?city='+this.city)
+            axios.get("http://wthrcdn.etouch.cn/weather_mini?city="+this.city)
             .then(function(response){
-                // console.log(response);
                 console.log(response.data.data.forecast);
                 that.weatherList = response.data.data.forecast
             })
             .catch(function(err){})
+         },
+         changeCity:function(city){
+             this.city = city;
+             this.searchWeather();
          }
-     },
+     }
  })
--- demo-悦听player/js/main.js
+++ demo-悦听player/js/main.js
@@ -27,98 +27,21 @@
     响应内容:mv的地址
 */
 var app = new Vue({
-  el: "#player",
-  data: {
-    // 查询关键字
-    query: "",
-    // 歌曲数组
-    musicList: [],
-    // 歌曲地址
-    musicUrl: "",
-    // 歌曲封面
-    musicCover: "",
-    // 歌曲评论
-    hotComments: [],
-    // 动画播放状态
-    isPlaying: false,
-    // 遮罩层的显示状态
-    isShow: false,
-    // mv地址
-    mvUrl: ""
+  el:"#player",
+  data:{
+    query:"",
+    musicList:[]
   },
-  methods: {
-    // 歌曲搜索
-    searchMusic: function() {
-      var that = this;
-      axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(
-        function(response) {
-          // console.log(response);
-          that.musicList = response.data.result.songs;
-          console.log(response.data.result.songs);
-        },
-        function(err) {}
-      );
-    },
-    // 歌曲播放
-    playMusic: function(musicId) {
-      //   console.log(musicId);
-      var that = this;
-      // 获取歌曲地址
-      axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.data[0].url);
-          that.musicUrl = response.data.data[0].url;
-        },
-        function(err) {}
-      );
-
-      // 歌曲详情获取
-      axios.get("https://autumnfish.cn/song/detail?ids=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.songs[0].al.picUrl);
-          that.musicCover = response.data.songs[0].al.picUrl;
-        },
-        function(err) {}
-      );
-
-      // 歌曲评论获取
-      axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.hotComments);
-          that.hotComments = response.data.hotComments;
-        },
-        function(err) {}
-      );
-    },
-    // 歌曲播放
-    play: function() {
-      // console.log("play");
-      this.isPlaying = true;
-    },
-    // 歌曲暂停
-    pause: function() {
-      // console.log("pause");
-      this.isPlaying = false;
-    },
-    // 播放mv
-    playMV: function(mvid) {
-      var that = this;
-      axios.get("https://autumnfish.cn/mv/url?id=" + mvid).then(
-        function(response) {
-          // console.log(response);
-          console.log(response.data.data.url);
-          that.isShow = true;
-          that.mvUrl = response.data.data.url;
-        },
-        function(err) {}
-      );
-    },
-    // 隐藏
-    hide: function() {
-      this.isShow = false;
+  methods:{
+    searchMusic:function(){
+      axios.get("https://autumnfish.cn/search?keywords="+this.query)
+      .then(res => {
+        // console.log(res);
+        this.musicList = res.data.result.songs;
+      })
+      .catch(err => {
+        console.log(err);
+      })
     }
   }
-});
+})
